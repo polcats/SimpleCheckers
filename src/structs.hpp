58dc4659671c0da812fe97d0bbd203044b93af9c
Paul Catalan <contact.pcatl@gmail.com>
2019-12-01 14:11:44 +0800

    Internal validity checking in Coordinates struct.

--- src/structs.hpp
+++ src/structs.hpp
@@ -12,16 +12,47 @@ struct Piece
 
 struct Coordinate
 {
-    Coordinate(uint8_t x, uint8_t y) : x(x), y(y) {}
+    Coordinate(int8_t x, int8_t y) : x(x), y(y) {}
     int8_t x;
     int8_t y;
+    // bool inBounds = true;
+
+    Coordinate getTopRight()
+    {
+        return Coordinate(x + 1, y - 1);
+    }
+    Coordinate getTopLeft()
+    {
+        return Coordinate(x - 1, y - 1);
+    }
+    Coordinate getBottomRight()
+    {
+        return Coordinate(x + 1, y + 1);
+    }
+    Coordinate getBottomLeft()
+    {
+        return Coordinate(x - 1, y + 1);
+    }
+    bool isValid()
+    {
+        if (x > 7 || x == -1 || y > 7 || y == -1)
+        {
+            return false;
+        }
+
+        return true;
+    }
 };
 
 struct Move
 {
-    Move(uint8_t sx, uint8_t sy, uint8_t dx, uint8_t dy) : src(sx, sy), des(dx, dy)
+    Move(uint8_t sx, uint8_t sy, uint8_t dx, uint8_t dy) :
+        src(sx, sy),
+        des(dx, dy)
     {}
-    Move(Coordinate src, Coordinate des) : src(src), des(des)
+    Move(Coordinate src, Coordinate des) :
+        src(src),
+        des(des)
     {}
     Coordinate src;
     Coordinate des;
