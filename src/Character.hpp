9c7e2a8f44dbd9a0008150f1308dc2746ce7b88b
Paul Catalan <contact.pcatl@gmail.com>
2019-12-01 11:41:07 +0800

    Added Character class.

---
+++ src/Character.hpp
@@ -0,0 +1,49 @@
+#pragma once
+
+#include "enums.hpp"
+
+class Character
+{
+public:
+    std::string getTile(Color c)
+    {
+        if (c)
+        {
+            return "\u2610";
+        }
+
+        return "\u25FC";
+    }
+
+    std::string getImage(std::unique_ptr<Piece>& p)
+    {
+        Rank& rank = p->rank;
+        Color& color = p->color;
+
+        switch (rank)
+        {
+            case NORMAL:
+            {
+                if (color)
+                {
+                    return "\u2688";
+                }
+
+                return "\u2686";
+            }
+            case UPGRADED:
+            {
+                if (color)
+                {
+                    return "\u2689";
+                }
+
+                return "\u2587";
+            }
+            default:
+            {
+                return "";
+            }
+        }
+    }
+};
